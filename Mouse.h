66b665fa1066be0965e574b1fc21711dd6191b7a
Mitch Barnett <mitch@mitchbarnett.com>
2017-02-16 23:37:49 +0000

    Added doxygen comments

--- Mouse.h
+++ Mouse.h
@@ -1,13 +1,50 @@
 #pragma once
 #include <Windows.h>
+
+/*!
+Give access to the real-time state of the mouse.
+
+Provides access to the position of the mouse.
+Only contains static functions as there should only be a single mouse (should not be instantiated).
+*/
 class Mouse
 {
 public:
-	Mouse();
-	~Mouse();
+	/*!
+	Gets the position of the mouse in global coordinates.
+
+	This function returns the global position of the mouse cursor on the desktop.
+	\return Current position of the mouse in global coordinates.
+	*/
 	static POINT getPosition();
+
+	/*!
+	Gets the position of the mouse in window coordinates.
+
+	This function returns the position of the mouse relative to the
+	top left of the given window.
+	\param window The handle of the window to use as a reference.
+	\return Current position of the mouse in window coordinates.
+	*/
 	static POINT getPosition(HWND& window);
+
+	/*!
+	Sets the cursor to the given global coordinates.
+
+	This function sets the global position of the  mouse cursor on the screen.
+	\param position The position to set the cursor to.
+	*/
 	static void setPosition(const POINT& position);
+
+	/*!
+	Sets the cursor position relitive to a window.
+
+	Sets the cursor to the given position relative to the top left of the
+	given window.
+	This function sets the global position of the  mouse cursor on the screen.
+	\param position The position to set the cursor to.
+	\param window The handle of the window to use as a reference.
+	*/
 	static void setPosition(const POINT& position, const HWND& window);
 
 
