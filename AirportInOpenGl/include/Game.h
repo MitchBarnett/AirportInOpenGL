ea3d8b3c08c97a2c9560c28eed8249bbcd4946dc
Mitch Barnett <mitch@mitchbarnett.com>
2017-02-24 19:13:54 +0000

    Scene expansion

--- AirportInOpenGl/include/Game.h
+++ AirportInOpenGl/include/Game.h
@@ -44,6 +44,7 @@ public:
 	void DestroyGLWindow();
 
 	void PrepareToDraw();
+	void reloadScene();
 	void Draw();
 
 	void Update();
--- AirportInOpenGl/src/Scene.cpp
+++ AirportInOpenGl/src/Scene.cpp
@@ -11,6 +11,8 @@ Scene::Scene()
 
 void Scene::load()
 {
+	m_models = {};
+	m_objects = {};
 	m_models.insert(std::pair<string, Model>("errModel", Model("Models\\simpleCubeWithTextures2.obj", "Textures\\redBrick256x256.bmp")));
 	pugi::xml_document doc;
 	pugi::xml_parse_result parseResult = doc.load_file("Scenes\\scene.xml");
@@ -50,8 +52,10 @@ void Scene::load()
 
 	pugi::xml_node objectsTag = doc.child("scene").child("objects");
 	// tag::basic[]
+	int objectCount = 0;
 	for (pugi::xml_node object = objectsTag.first_child(); object; object = object.next_sibling())
 	{
+		objectCount++;
 		Model*	model = &m_models["errModel"];
 		vec3	position = { 0,0,0 };
 		vec3	rotation = { 0,0,0 };
@@ -62,33 +66,41 @@ void Scene::load()
 		}
 		else
 		{
+			Log::AppendToLogFile("Creating object #: " + to_string(objectCount) + "\" failed");
 			Log::AppendToLogFile("no model named: \"" + string(object.attribute("model").value()) + "\" found");
 		}
 		pugi::xml_node positionNode = object.child("position");
 		pugi::xml_node rotationNode = object.child("rotation");
 		pugi::xml_node scaleNode = object.child("scale");
 
-		if (positionNode)
+		try
 		{
-			position.v[0] = stof(positionNode.attribute("x").value());
-			position.v[1] = stof(positionNode.attribute("y").value());
-			position.v[2] = stof(positionNode.attribute("z").value());
-		}
+			if (positionNode)
+			{
+				position.v[0] = stof(positionNode.attribute("x").value());
+				position.v[1] = stof(positionNode.attribute("y").value());
+				position.v[2] = stof(positionNode.attribute("z").value());
+			}
 
-		if (rotationNode)
-		{
-			rotation.v[0] = stof(rotationNode.attribute("x").value());
-			rotation.v[1] = stof(rotationNode.attribute("y").value());
-			rotation.v[2] = stof(rotationNode.attribute("z").value());
-		}
-		
-		if (scaleNode)
-		{
-			scale.v[0] = stof(scaleNode.attribute("x").value());
-			scale.v[1] = stof(scaleNode.attribute("y").value());
-			scale.v[2] = stof(scaleNode.attribute("z").value());
-		}
+			if (rotationNode)
+			{
+				rotation.v[0] = stof(rotationNode.attribute("pitch").value());
+				rotation.v[1] = stof(rotationNode.attribute("roll").value());
+				rotation.v[2] = stof(rotationNode.attribute("yaw").value());
+			}
 
+			if (scaleNode)
+			{
+				scale.v[0] = stof(scaleNode.attribute("x").value());
+				scale.v[1] = stof(scaleNode.attribute("y").value());
+				scale.v[2] = stof(scaleNode.attribute("z").value());
+			}
+		}
+		catch(std::invalid_argument& e)
+		{
+			Log::AppendToLogFile("Creating object #: " + to_string(objectCount) +  " with model \"" + string(object.attribute("model").value()) + "\" failed");
+			Log::AppendToLogFile("one or more values could not be converted to float type");
+		}
 		m_objects.push_back(ModelInstance(model, position, rotation, scale));
 	}
 	Log::AppendToLogFile(to_string(m_objects.size()) + " Objects loaded");
